8d47444a7dcf6407209ee6040d718c5f5711166f
Sasidharan Amp Manakkattu Kandathil <ampu@Sasidharans-Mac-mini.local>
2021-07-23 21:41:37 +0530

    Commit the LogProject

---
+++ LogTest/src/main/java/com/ampu/logging/LoggingSample.java
@@ -0,0 +1,19 @@
+package com.ampu.logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingSample {
+	private static final Logger LOGGER = LogManager.getLogger(LoggingSample.class);
+
+	public static void main(String[] args) {
+		LOGGER.debug("This will be printed on debug");
+		LOGGER.info("This will be printed on info");
+		LOGGER.warn("This will be printed on warning");
+		LOGGER.error("This will be printed on error");
+		LOGGER.fatal("This will be printed on fatal");
+		LOGGER.info("Appending string: {}.", "Hello, World");
+
+	}
+
+}
