b655918f4ca62ccfa47a30834035c3b6b529d99d
ampu-dev <87851907+ampu-dev@users.noreply.github.com>
2021-07-23 13:57:46 +0530

    Update Test5.java
    Test3

--- src/com/ampu/test/Test5.java
+++ src/com/ampu/test/Test5.java
@@ -4,7 +4,7 @@ public class Test5 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Europe");
-		System.out.println("Hello UK");
+		System.out.println("Hello France");
 		System.out.println("Hello Germany");
 
 	}
